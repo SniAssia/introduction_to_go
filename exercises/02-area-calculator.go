6bad33ac0eaf0d15f89ce4b8011a1640651c31e1
SniAssia <assia.snissi@um6p.ma>
2026-01-05 11:22:17 +0100

    Create 02-area-calculator.go

---
+++ exercises/02-area-calculator.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+		
+
+func main() {
+	
+	var l1 = 12.2
+	var l2 =14.9
+	var prod = l1*l2
+	fmt.Println("the area of this rectangle is : ",prod,"m")
+	var foot = prod*3.28
+	fmt.Println("the area of this rectangle is : ",foot,"foot")
+
+
+}
